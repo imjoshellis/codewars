062369d69f271b6ed9234090a82bb4cf2ca37bd0
Josh Ellis <josh@imjoshellis.com>
2020-11-21 08:24:11 -0600

    Update script

--- scripts/new.js
+++ scripts/new.js
@@ -3,38 +3,46 @@ import inquire from 'inquirer'
 
 const Main = async () => {
   let url = process.argv[2]
+  let name = process.argv[3]
   if (!url) {
     const { input } = await inquire.prompt([
-      {
-        type: 'input',
-        name: 'input',
-        message: 'url: '
-      }
+      { type: 'input', name: 'input', message: 'url: ' }
     ])
     url = input
   }
 
-  const name = url
-    .split('/')
-    .filter(w => !!w)
-    .reverse()[0]
-    .split('-')
-    .map((w, i) => {
-      const word = [...w]
-      word[0] = i === 0 ? word[0] : word[0].toUpperCase()
-      return word.join('')
-    })
-    .slice(0, 3)
-    .join('')
+  if (!name) {
+    const { input } = await inquire.prompt([
+      { type: 'input', name: 'input', message: 'name: ' }
+    ])
+    name = input
+  }
+
+  if (!name) {
+    name = url
+      .split('/')
+      .filter(w => !!w)
+      .reverse()[0]
+      .split('-')
+      .map((w, i) => {
+        const word = [...w]
+        word[0] = i === 0 ? word[0] : word[0].toUpperCase()
+        return word.join('')
+      })
+      .slice(0, 3)
+      .join('')
+  }
 
   const filename = `working/${name}`
 
   fs.writeFile(
     `${filename}.ts`,
     `// ${url}
-    export const ${name} = (arg: string) => {
+    const ${name} = (arg: string) => {
       return arg
-    }`,
+    }
+    
+    export { ${name} }`,
     err => {
       if (err) throw err
       console.log('Saved!')
