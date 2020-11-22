4ecc18230ef91764c26328135d97375963f91516
Josh Ellis <josh@imjoshellis.com>
2020-11-21 18:14:07 -0600

    Solved

---
+++ solved/__test__/generateMatrix.test.ts
@@ -0,0 +1,5 @@
+import { generateMatrix } from './generateMatrix'
+
+it('does nothing', () => {
+  expect(generateMatrix(3)).toBe([[1]])
+})
---
+++ solved/generateMatrix.ts
@@ -0,0 +1,47 @@
+// amazon-interview-mock
+const generateMatrix = (n: number): number[][] => {
+  if (!n) {
+    return []
+  }
+  let startRow = 0
+  let endRow = n - 1
+  let startColumn = 0
+  let endColumn = n - 1
+  let matrix: number[][] = []
+  let counter = 1
+  while (startRow <= endRow && startColumn <= endColumn) {
+    for (let i = startColumn; i <= endColumn; i++) {
+      if (!matrix[startRow]) {
+        matrix[startRow] = []
+      }
+      matrix[startRow][i] = counter++
+    }
+    startRow++
+    for (let i = startRow; i <= endRow; i++) {
+      if (!matrix[i]) {
+        matrix[i] = []
+      }
+      matrix[i][endColumn] = counter++
+    }
+    endColumn--
+    if (startRow <= endRow && startColumn <= endColumn) {
+      for (let i = endColumn; i >= startColumn; i--) {
+        if (!matrix[endRow]) {
+          matrix[endRow] = []
+        }
+        matrix[endRow][i] = counter++
+      }
+      endRow--
+      for (let i = endRow; i >= startRow; i--) {
+        if (!matrix[i]) {
+          matrix[i] = []
+        }
+        matrix[i][startColumn] = counter++
+      }
+      startColumn++
+    }
+  }
+  return matrix
+}
+
+export { generateMatrix }
