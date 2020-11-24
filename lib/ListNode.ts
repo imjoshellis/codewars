f090304e236f921f84ac4cb2f1b197c63c648f6d
Josh Ellis <josh@imjoshellis.com>
2020-11-23 19:07:05 -0600

    Solved

--- lib/ListNode.ts
+++ lib/ListNode.ts
@@ -1,9 +1,13 @@
-export type NullableListNode = ListNode | null
+export type MaybeNode = ListNode | null
 export class ListNode {
   val: number
-  next: NullableListNode
-  constructor (val?: number, next?: NullableListNode) {
+  next: MaybeNode
+  constructor (val?: number, next?: MaybeNode) {
     this.val = val === undefined ? 0 : val
     this.next = next === undefined ? null : next
   }
+}
+
+export const arrayToList = (a: number[]): MaybeNode => {
+  return a.length ? new ListNode(a.pop(), arrayToList(a)) : null
 }
--- solved/getIntersectionNode.ts
+++ solved/getIntersectionNode.ts
@@ -1,14 +1,11 @@
-import { NullableListNode } from '../lib/ListNode'
+import { MaybeNode } from '../lib/ListNode'
 
 // https://leetcode.com/explore/learn/card/linked-list/214/two-pointer-technique/1215/
-const getIntersectionNode = (
-  headA: NullableListNode,
-  headB: NullableListNode
-): NullableListNode => {
+const getIntersectionNode = (headA: MaybeNode, headB: MaybeNode): MaybeNode => {
   if (headA == null || headB == null) return null
 
-  let a: NullableListNode = headA
-  let b: NullableListNode = headB
+  let a: MaybeNode = headA
+  let b: MaybeNode = headB
 
   while (a != b) {
     a = a === null ? headB : a.next
