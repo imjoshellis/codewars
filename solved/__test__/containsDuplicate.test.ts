1f2326f0190ab95108e938cf85488e66b86e3f2f
Josh Ellis <josh@imjoshellis.com>
2020-11-26 08:41:41 -0600

    Solved

--- solved/__test__/containsDuplicate.test.ts
+++ solved/__test__/containsDuplicate.test.ts
@@ -1,11 +1,16 @@
 import { containsDuplicate } from './containsDuplicate'
 
-it('returns false if no dups', () => {
-  const test = [1, 2]
+it('does nothing', () => {
+  const test = [] as number[]
   expect(containsDuplicate(test)).toBe(false)
 })
 
-it('returns true if dups', () => {
-  const test = [1, 3, 3]
+it('returns false when no dups', () => {
+  const test = [1, 2, 3]
+  expect(containsDuplicate(test)).toBe(false)
+})
+
+it('returns true when dups', () => {
+  const test = [1, 2, 3, 3]
   expect(containsDuplicate(test)).toBe(true)
 })
--- solved/containsDuplicate.ts
+++ solved/containsDuplicate.ts
@@ -1,4 +1,11 @@
-export const containsDuplicate = (arg: number[]): boolean => {
-  const set = new Set(arg)
-  return set.size !== arg.length
+// https://leetcode.com/explore/featured/card/top-interview-questions-easy/92/array/578/
+const containsDuplicate = (nums: number[]): boolean => {
+  const map = new Map()
+  for (const n of nums) {
+    if (map.has(n)) return true
+    map.set(n, true)
+  }
+  return false
 }
+
+export { containsDuplicate }
