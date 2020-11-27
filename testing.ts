2f9d4078f410dbac8d03d7e1e73a722fd116c59f
Josh Ellis <josh@imjoshellis.com>
2020-11-27 11:03:17 -0600

    Delete testing.ts

--- testing.ts
+++
@@ -1,5 +0,0 @@
-const findMaxConsecutiveOnes = (nums: number[]): number =>
-  nums.reduce(reducer, [0, 0])[0]
-
-const reducer = ([max, cur]: number[], n: number): number[] =>
-  n === 1 ? [Math.max(max, cur + 1), cur + 1] : [max, 0]
