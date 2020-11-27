df50a058803f475dd270c1ca3fa6f6d229ba3253
Josh Ellis <josh@imjoshellis.com>
2020-11-27 11:03:21 -0600

    Update isValidSudoku.ts

--- solved/isValidSudoku.ts
+++ solved/isValidSudoku.ts
@@ -1,14 +1,14 @@
 // https://leetcode.com/explore/featured/card/top-interview-questions-easy/92/array/769/
 const isValidSudoku = (board: string[][]): boolean => {
   const seen: { [key: string]: boolean } = {}
-  const calcBox = (i: number, j: number): number =>
+  const getBox = (i: number, j: number): number =>
     Math.floor(i / 3) + Math.floor(j / 3) * 3
 
   for (let i = 0; i < board.length; i++) {
     for (let j = 0; j < board[i].length; j++) {
       const n = parseInt(board[i][j])
       if (isNaN(n)) continue
-      const b = calcBox(i, j)
+      const b = getBox(i, j)
       const colKey = `col-${j}-${n}`
       const rowKey = `row-${i}-${n}`
       const boxKey = `box-${b}-${n}`
