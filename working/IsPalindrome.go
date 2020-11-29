09ada7a5427459fe17c39bb0a336096374b25bb6
Josh Ellis <josh@imjoshellis.com>
2020-11-29 16:44:50 -0600

    Started

---
+++ working/IsPalindrome.go
@@ -0,0 +1,12 @@
+// Package main has solved algorithms
+    // https://leetcode.com/explore/interview/card/top-interview-questions-easy/93/linked-list/772/
+    package main
+    
+    // import log "github.com/sirupsen/logrus"
+    
+    func isPalindrome() {
+    }
+    
+    func main() {
+      // log.Info(isPalindrome())
+    }
