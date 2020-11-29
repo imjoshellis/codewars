456c1871782ca3ef34549ba91377bbe76c9c797c
Josh Ellis <josh@imjoshellis.com>
2020-11-29 17:11:38 -0600

    Solved

--- solved/IsPalindrome.go
+++ solved/IsPalindrome.go
@@ -1,24 +1,43 @@
-// Package algos has solved algorithms
-// https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/883/
-package algos
+// Package main has solved algorithms
+// https://leetcode.com/explore/interview/card/top-interview-questions-easy/93/linked-list/772/
+package main
 
-import (
-	"regexp"
-	"strings"
-)
+// import log "github.com/sirupsen/logrus"
 
-func isPalindrome(s string) bool {
-	s = strings.ToLower(s)
-	r := regexp.MustCompile("[^a-z0-9]+")
-	s = r.ReplaceAllString(s, "")
-	start := 0
-	end := len(s) - 1
-	for start < end {
-		if s[start] != s[end] {
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func isPalindrome(head *ListNode) bool {
+	if head == nil || head.Next == nil {
+		return true
+	}
+
+	st := make([]int, 0)
+	slow := head
+	fast := head
+
+	for fast != nil && fast.Next != nil {
+		st = append(st, slow.Val)
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+
+	if fast != nil {
+		slow = slow.Next
+	}
+
+	for i := len(st) - 1; i >= 0; i-- {
+		if st[i] != slow.Val {
 			return false
 		}
-		start++
-		end--
+		slow = slow.Next
 	}
+
 	return true
+}
+
+func main() {
+	// log.Info(isPalindrome())
 }
--- solved/__test__/IsPalindrome_test.go
+++ solved/__test__/IsPalindrome_test.go
@@ -1,4 +1,12 @@
-package algos
+package main
     
-    func TestIsPalindrome() {
+    func TestIsPalindrome(t *testing.T) {
+      exp := []string{"1"}
+      args := []int{1}
+      for i, arg := range args {
+        res := isPalindrome(arg)
+        if res != exp[i] {
+          t.Fatalf("Expected %v, got %v with %v arg", exp[i], res, arg)
+        }
+      }
     }
