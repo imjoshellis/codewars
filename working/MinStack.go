deca55273f2882d189bd96c0762999b78bac42a9
Josh Ellis <josh@imjoshellis.com>
2020-12-02 13:19:24 -0600

    Started MinStack in go

---
+++ working/MinStack.go
@@ -0,0 +1,8 @@
+// Package minstack solves X problem from Y
+// URL: https://leetcode.com/problems/min-stack/
+package minstack
+
+// MinStack is the primary function for the problem
+func MinStack(s string) (string, error) {
+	return s, nil
+}
---
+++ working/README.md
@@ -0,0 +1,3 @@
+# MinStack
+
+🛑️ This won't make any sense unless you're familiar with the problem explanation on LeetCode: <https://leetcode.com/problems/min-stack/>
