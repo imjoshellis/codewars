b9215fdaf8935e0d35dd15620deb6c01fefeeaea
Josh Ellis <josh@imjoshellis.com>
2020-12-02 19:23:00 -0600

    Started isValid in go

--- working/README.md
+++ working/README.md
@@ -1,1 +1,3 @@
+# isValid
 
+🛑️ This won't make any sense unless you're familiar with the problem explanation on LeetCode: <https://leetcode.com/problems/valid-parentheses>
---
+++ working/isValid.go
@@ -0,0 +1,8 @@
+// Package isvalid solves X problem from Y
+// URL: https://leetcode.com/problems/valid-parentheses
+package isvalid
+
+// isValid is the primary function for the problem
+func isValid(s string) (string, error) {
+	return s, nil
+}
